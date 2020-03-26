277afb8ed23fc2dc3dee30d0c8fd11dd248dad74
deewooley <55896326+deewooley@users.noreply.github.com>
2020-03-26 12:52:15 -0400

    Info on Operators

--- learnPython.py
+++ learnPython.py
@@ -1,4 +1,4 @@
-print("Hello, I am now Learning Python!");
+print("Hello, I am now Learning Python!")
 if 5 > 2:
      print("Five is greater than two!") 
 x = 5
@@ -78,4 +78,23 @@ x = bytearray(5)	bytearray
 x = memoryview(bytes(5))	memoryview
 
 NEXT Python Numbers W3
+
+Operators Remember:
+
+**Exponentation
+//Floor Division
+
+Logical Operators = and, or, not
+Identity Operators= is(x is y), is not(x is not y)
+Membership operators = in(ret True if value is present in the object), not it(ret true if a seq w a spec value is not present in the object)
+Bitwise Operators = &, |, ^(XOR - 	Sets each bit to 1 if only one of two bits is 1), ~(NOT), <<(Zero fill left shift), >>(Zero fill right shift)
+_______________
+(12 >> 1) - 1)         
+>> shifts binary 12(1100) 1 times to the right.
+12 >> 1 == 6(110)
+
+6 - 1 == 5
+
+
+Continue with python lists
 """
